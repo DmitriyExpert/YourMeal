94846dea400c138b615db184d3ac2d6a6339bcfc
Demakov Dmitry <dpemvr@yandex.ru>
2025-07-03 16:23:50 +0500

    Adding basket layout and catalog menu

---
+++ src/js/category.js
@@ -0,0 +1,19 @@
+const categoryBtn = document.querySelectorAll('.category-btn');
+
+const activateButton = function(button) {
+  button.classList.add('active');
+}
+
+const disactivateButton = function(button) {
+  button.classList.remove('active');
+}
+
+categoryBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const activeBtn = document.querySelector('.category-btn.active');
+    if(activeBtn) {
+      disactivateButton(activeBtn)
+    }
+    activateButton(button)
+  })
+})
